eaa48822baf01ac84884c2f0146ed23c2a0b97e7
Dmitry-S85 <dmitry.stoyankin@yandex.ru>
2024-09-08 17:14:13 +0300

    Add files via upload

---
+++ module_6_2.py
@@ -0,0 +1,53 @@
+class Vehicle:
+    __COLOR_VARIANTS = ['red', 'green', 'blue', 'BLACK']
+
+    def __init__(self):
+         self.__color = None
+
+    def __init__(self, owner, model, engine_power, color):
+        self.owner = owner
+        self.__model = model
+        self.__engine_power = engine_power
+        self.__color = color
+
+    def get_model(self):
+        return self.__model
+
+    def get_horsepower(self):
+        return self.__engine_power
+
+    def get_color(self):
+        return self.__color
+
+    def set_color(self, new_color):
+        if new_color in self.__COLOR_VARIANTS:
+            self.__color = new_color
+        else:
+            print("Нельзя сменить цвет на " + new_color)
+
+    def print_info(self):
+        print(f'Модель: {self.__model}')
+        print(f'Мощность двигателя: {self.__engine_power}')
+        print(f'Цвет: {self.__color}')
+        print(f'Владелец: {self.owner}')
+
+#
+class Sedan(Vehicle):
+    __PASSENGERS_LIMIT = 5
+
+# Текущие цвета
+__COLOR_VARIANTS = ['blue', 'red', 'green', 'black', 'white']
+vehicle1 = Sedan('Fedos', 'Toyota Mark II', 500, 'blue')
+
+# Изначальные свойства
+vehicle1.print_info()
+
+# Меняем свойства (в т.ч. вызывая методы)
+vehicle1.set_color('Pink')
+vehicle1.set_color('BLACK')
+vehicle1.owner = 'Vasyok'
+
+vehicle1.print_info()
+
+
+
